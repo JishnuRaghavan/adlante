d703e49220812d822eb0f476927f1dbf70cc5e3a
JishnuRaghavan <jishu18js@gmail.com>
2024-08-14 20:52:40 +0530

    added basic home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, aliquid nostrum quo eum iure itaque laboriosam repudiandae a quos quis deleniti harum, nemo eligendi commodi, odio quibusdam. Sapiente, culpa consectetur.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, aliquid nostrum , nemo eligendi commodi, odio quibusdam. Sapiente, culpa consectetur.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adiibusdam. Sapiente, culpa consectetur.</p>
+
     </div>
   )
 }
